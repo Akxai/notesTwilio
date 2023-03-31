638af48e1737ef3038fa091fa1c5e2ba8c2c79be
Akshay Aileni <akshayakshay5543@gmail.com>
2023-03-31 19:06:23 +0530

    fix:twilio message

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 const accountSid = process.env.ACCOUNTSID;
 const authToken = process.env.AUTHTOKEN;
@@ -22,55 +24,54 @@ const authToken = process.env.AUTHTOKEN;
 const client = new twilio(accountSid, authToken);
 
 mongoose.connect(process.env.LINK, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
 });
 
 const querySchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true
+    required: true,
   },
   message: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 });
-
-
 
 const Message = mongoose.model("Message", querySchema);
 
-
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.render("index");
-})
+});
 
-app.get("/query", function(req, res) {
+app.get("/query", function (req, res) {
   res.render("query");
-})
+});
 
-app.post("/query", function(req, res) {
+app.post("/query", function (req, res) {
   const query1 = new Message({
     email: req.body.email,
-    message: req.body.name
+    message: req.body.name,
   });
   query1.save();
+  console.log("twilio");
   client.messages
-  .create({
-    body: `Email: ${req.body.email} Message: ${req.body.name}`,
-    to: process.env.ADMINNUMBER, // Text this number
-    from: process.env.FROMNUMBER, // From a valid Twilio number
-  });
+    .create({
+      body: `Email: ${req.body.email} Message: ${req.body.name}`,
+      to: process.env.ADMINNUMBER, // Text this number
+      from: process.env.FROMNUMBER, // From a valid Twilio number
+    })
+    .then((message) => console.log(message.sid));
   res.redirect("/");
 });
 
-app.get("/error", function(req, res) {
+app.get("/error", function (req, res) {
   res.render("error");
-})
-app.post("/error", function(req, res) {
+});
+app.post("/error", function (req, res) {
   res.redirect("/");
-})
+});
 
-app.listen(process.env.PORT || "3000", function() {
+app.listen(process.env.PORT || "3000", function () {
   console.log("server is running on port 3000");
 });
